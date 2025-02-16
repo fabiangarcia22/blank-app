5c8e35ddc1fde21768e85a5fc8de963f7737ba62
fabiangarcia22 <garciafabian22@yahoo.com>
2025-02-16 10:04:11 +0000

    first test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Cyber Attacks")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Views of the current threats and attemps made againts the Firewall"
 )
